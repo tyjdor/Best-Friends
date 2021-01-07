22644965fba03885d4c88ba4905e63f96fb95491
tyjdor <tyjdor96@gmail.com>
2021-01-06 20:33:54 -0500

    Add Menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+
+export default function App() {
+ 
+
   return (
-    <>  
-      <p>The app is done</p>
-    </>
+    <div style={{textAlign:'center'}}>
+      <Button>About</Button>
+      <Button color="primary">Explore</Button>
+      <Button color="secondary">Your BFFs</Button>
+      <Button  color="primary">Contact</Button>
+    </div>
   );
 }
-
-export default App;
