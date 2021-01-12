ad4af5c07e9e1ac2fead022e2e458c0d42e24249
tyjdor <tyjdor96@gmail.com>
2021-01-12 11:55:43 -0500

    Fix login auth
    Co-Authored-By: Adam Jeniski <45019515+Ajetski@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { connect } from 'react-redux';
 import BFF from './Components/bff';
 import About from './Components/about';
 import Explore from './Components/explore';
@@ -6,40 +9,41 @@ import Contact from './Components/contact';
 import Login from './Components/login';
 import Logout from './Components/logout';
 import GuardedRoute from './Components/guardroute';
-import { Button } from '@material-ui/core';
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Unauthorized from './Components/unauthorized';
 
-export default function App() {
+function App({ authenticated }) {
+
 	return (
 		<>
+
 			<Router>
 				<div style={{ textAlign: 'center' }}>
-					<Button><Link to="/about">About</Link></Button>
+					<Button><Link to="/">About</Link></Button>
 					<Button><Link to="/explore">Explore</Link></Button>
 					<Button><Link to="/bff">Your BFFs</Link></Button>
 					<Button><Link to="/contact">Contact</Link></Button>
 					<Switch>
-						<Route exact path="/about">
-							<About />
-						</Route>
-						<Route path="/explore">
-							<Explore />
-						</Route>
-						<Route path="/bff" >
-							<BFF />
-						</Route>
-						<Route path="/contact">
-							<Contact />
-						</Route>
+						<Route exact path="/" component={About} />
+						<Route path="/about" component={About} />
+						<Route path="/explore" component={Explore} />
+						<GuardedRoute path='/bff' component={BFF} auth={authenticated} />
+						<Route path="/contact" component={Contact} />
+						<Route path="/unauthorized" component={Unauthorized} />
+						<Route path="/" component={Notfound} />
 					</Switch>
-					<GuardedRoute path='/bff' component={BFF} auth={false} />
-
 				</div>
 			</Router>
-
 			<Login />
 			<Logout />
 
 		</>
 	);
 }
+
+
+
+export default connect((state /*, ownProps*/) => {
+	return {
+		authenticated: state.authenticated
+	};
+})(App);
