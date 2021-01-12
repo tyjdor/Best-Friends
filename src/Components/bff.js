8f23a7137188e2959ad4dab8fc42c93eec147d00
tyjdor <tyjdor96@gmail.com>
2021-01-12 11:55:50 -0500

    Update bff.js

--- src/Components/bff.js
+++ src/Components/bff.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+
 export default function BFF() {
-    return (
-      <div>
-        <h2>Your BFFs</h2>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Your BFFs</h2>
+    </div>
+  );
+}
+
